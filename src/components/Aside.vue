04f491432ada35751f1f591bef914d0b2e0d06c6
peng <17685217897@126.com>
2022-12-09 22:20:50 +0800

    增加导航栏工作区树形目录的动态添加和删除

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -1,84 +1,81 @@
 <template>
   <div>
     <el-container>
-      <el-aside width="200px">
-        <el-container>
-          <el-header height="80px">
-            <div style="float: left;margin-top: 20px;">
-              <el-image :src="src"></el-image>
-              <div style="float: right;margin-left: 20px;margin-top: 0;line-height: 25px">
-                <span>One Start</span>
-                <br/>
-                <span>admin</span>
-              </div>
-            </div>
-          </el-header>
-          <el-main>
-            <div class="menu">
-              <el-menu
-                  ref="menu"
-                  @select="menuItemClick"
-                  default-active="1"
-              >
-                <el-menu-item index="1">
-                  <template slot="title">
-                    <i class="el-icon-s-home"></i>
-                    <span slot="title">主页</span>
-                  </template>
-                </el-menu-item>
-                <el-menu-item index="/test">
-                  <i class="el-icon-star-off"></i>
-                  <span slot="title">加星</span>
-                </el-menu-item>
-                <el-menu-item index="editor">
-                  <i class="el-icon-folder"></i>
-                  <span slot="title">我的文件夹</span>
-
-                  <el-dropdown trigger="click" placement="bottom-start">
+      <el-header height="80px">
+        <div class="header">
+          <el-image :src="src"></el-image>
+          <div style="float: right;margin-left: 20px;margin-top: 0;line-height: 25px">
+            <span>One Start</span>
+            <br/>
+            <span>admin</span>
+          </div>
+        </div>
+      </el-header>
+      <el-main>
+        <div class="menu">
+          <el-menu
+              ref="menu"
+              @select="menuItemClick"
+              :default-active="dfa"
+          >
+            <el-menu-item index="/home">
+              <template slot="title">
+                <i class="el-icon-s-home"></i>
+                <span slot="title">主页</span>
+              </template>
+            </el-menu-item>
+            <el-menu-item index="/test">
+              <i class="el-icon-star-off"></i>
+              <span slot="title">加星</span>
+            </el-menu-item>
+            <el-menu-item index="editor">
+              <i class="el-icon-folder"></i>
+              <span slot="title">我的文件夹</span>
+            </el-menu-item>
+          </el-menu>
+          <div class="workspace">
+            <el-tree
+                ref="workspace"
+                :data="workspace"
+                :props="defaultProps"
+                :expand-on-click-node="false"
+                node-key="tag"
+                @node-click="handleNodeClick">
+               <span class="custom-tree-node" slot-scope="{ node, data }">
+                  <span>{{ node.label }}</span>
+                   <span>
+                    <el-button
+                        style="padding-right: 0;padding-left: 0"
+                        type="text"
+                        @click="() => append(data,node.parent)">
+                      <i class="el-icon-plus el-icon--left"></i>
+                    </el-button>
+                 <el-dropdown trigger="click" placement="bottom-start">
                   <span class="el-dropdown-link">
                    <i class="el-icon-more"></i>
                   </span>
                     <el-dropdown-menu slot="dropdown">
-                      <el-dropdown-item>新建文件夹</el-dropdown-item>
+                      <el-dropdown-item>
+                        <el-button
+                            style="padding-right: 0;padding-left: 0"
+                            type="text"
+                            size="mini"
+                            @click="() => remove(node, data)">
+                          <span style="font-size: 14px">删除</span>
+                        </el-button>
+                      </el-dropdown-item>
                       <el-dropdown-item>新建文档</el-dropdown-item>
                     </el-dropdown-menu>
                   </el-dropdown>
-                </el-menu-item>
-              </el-menu>
+                  </span>
+               </span>
+            </el-tree>
+
+            <div class="addWorkspace">
+              <el-button type="text" @click="addWorkSpace"><i class="el-icon-plus el-icon--left"></i>添加工作区</el-button>
+              <el-button type="text" @click="restore"><i class="el-icon-plus el-icon--left"></i>恢复</el-button>
             </div>
-          </el-main>
-        </el-container>
-      </el-aside>
-      <el-main>
-        <div class="secondAside">
-          <el-input
-              placeholder="输入关键字进行过滤"
-              style="width: 180px;margin-top: 10px"
-              v-model="filterText">
-          </el-input>
-          <el-tree
-              :data="data"
-              node-key="id"
-              :filter-node-method="filterNode"
-              :expand-on-click-node="false">
-      <span class="custom-tree-node" slot-scope="{ node, data }">
-        <span>{{ node.label }}</span>
-        <span>
-          <el-button
-              type="text"
-              size="mini"
-              @click="() => append(data)">
-            Append
-          </el-button>
-          <el-button
-              type="text"
-              size="mini"
-              @click="() => remove(node, data)">
-            Delete
-          </el-button>
-        </span>
-      </span>
-          </el-tree>
+          </div>
         </div>
       </el-main>
     </el-container>
@@ -93,29 +90,23 @@ export default {
   name: "Aside",
   data() {
     return {
+      dfa: "/home",
       activeNames: ['1'],
       src: 'https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg',
       fits: 'fill',
       filterText: '',
-      dist:'Docker/Docker.doc',
-      data: [{
-        id: 1,
-        label: '一级 1',
-        type:'folder',
-        children: [{
-          id: 4,
-          label: '二级 1-1',
-          children: [{
-            label: '三级 1-1-1'
-          }, {
-            label: '三级 1-1-2'
-          }]
-        }]
-      },],
+      dist: 'Docker/Docker.doc',
+      //只用操作一个数据，但是每次添加和删除都需要修改数据库里的信息;
+      workspace: [],
+      deleteNode: {
+        parent: {},
+        nodeData: {}
+      },
       defaultProps: {
         children: 'children',
         label: 'label',
-        type:'type',
+        type: 'type',
+        tag: ''
       }
     }
   },
@@ -124,20 +115,27 @@ export default {
     filterText(val) {
       this.$refs.tree.filter(val);
     },
-    $route(){
+    $route() {
       this.menuItemClick()
     }
   },
   methods: {
-    filterNode(value, data) {
-      if (!value) return true;
-      return data.label.indexOf(value) !== -1;
+    handleNodeClick(data, node) {
+      this.dfa = "/";
+      // console.log(this.$refs.workspace.getCurrentKey())
+      // console.log(data.tag)
+      // console.log(data);
+      //console.log(node);
+      // console.log(this.$refs.workspace.getCurrentNode().type)
+      // console.log(JSON.stringify(this.workspace))
     },
     menuItemClick(index) {
       this.$router.replace(index);
     },
-    append(data) {
-      const newChild = { id: id++, label: 'testtest', children: [] };
+    append(data, parentNode) {
+      console.log(data)
+      const uuid = Math.random().toString()
+      const newChild = {label: '新页面', children: [], nodeKey: uuid};
       if (!data.children) {
         this.$set(data, 'children', []);
       }
@@ -146,11 +144,58 @@ export default {
 
     remove(node, data) {
       const parent = node.parent;
+      this.deleteNode.parent = node.parent.data;
+      this.deleteNode.nodeData = node.data;
       const children = parent.data.children || parent.data;
       const index = children.findIndex(d => d.id === data.id);
       children.splice(index, 1);
+      //console.log(this.deleteNode)
+      //console.log(this.$refs.workspace.getCurrentKey())
+      this.$axios({
+        url: "http://localhost:8005/node",
+        method: 'post',
+        data: {
+          "parent": this.deleteNode.parent,
+          "nodeData": this.deleteNode.nodeData
+        }
+      })
     },
+
+    restore() {
+      const resParent = this.deleteNode.parent;
+      const resData = this.deleteNode.nodeData;
+      console.log(resParent)
+      // console.log(this.$refs.workspace.getNode(resParent).data.children)
+      this.$refs.workspace.getNode(resParent).data.children.push(resData)
+    },
+
+    addWorkSpace() {
+      this.workspace.push({
+        label: 'workspace1',
+        children: []
+      },)
+      console.log(this.workspace)
+    }
   },
+  created() {
+    this.workspace = [
+      {
+        label: 'workspace1',
+        type: 'path',
+        tag: '5231',
+        children: [{
+          label: "新页面",
+          type: 'path',
+          tag: '5813',
+          children: [{
+            label: "新页面",
+            type: 'note',
+            tag: '8521',
+          }]
+        }]
+      },
+    ]
+  }
 }
 </script>
 
@@ -167,60 +212,57 @@ html, body {
   display: none;
 }
 
+.el-header:hover {
+  outline: 0 !important;
+  border-radius: 0.8em;
+  @include font_color("aside_text-color");
+  background: #e7ebed !important;
+}
+
 .el-menu .el-menu-item:hover {
   outline: 0 !important;
-  color: #5b89fe !important;
-  background: #C0C4CC !important;
+  @include font_color("aside_text-color");
+  border-radius: 0.8em;
+  background: #e7ebed !important;
 }
 
 ::v-deep .el-menu .el-menu-item.is-active {
-  color: #5b89fe !important;
-  background: #C0C4CC !important;
+  @include font_color("aside_text-color");
+  border-radius: 0.8em;
+  background: #e7ebed !important;
 }
 
-.el-menu, .el-menu-item, {
+.el-menu, .el-menu-item, .el-tree {
   @include background_color("aside_color");
   @include font_color("aside_text-color");
   transition: background 1s, color 0.6s;
 }
 
-.secondAside {
-  border-right: 2px solid #e4e8ec;
-  text-align: center;
-  background-color: white;
-  position: absolute;
-  left: 200px;
-  width: 200px;
-  height: 713px;
-  overflow-x: hidden;
-  overflow-y: auto;
-  -webkit-overflow-scrolling: touch;
-  @include background_color("background_color");
-  @include font_color("aside_text-color");
-  transition: background 1s, color 0.6s;
+.el-header {
+  width: 220px;
+  margin-left: 10px;
 }
 
-.filter-tree {
-  padding-left: 15px;
-  width: 180px;
+.header {
+  float: left;
   margin-top: 20px;
-  @include background_color("background_color");
-  @include font_color("aside_text-color");
-  transition: background 1s, color 0.6s;
+  margin-left: 20px
 }
 
-.filter-tree::v-deep .el-tree-node__content:hover {
-  outline: 0 !important;
-  color: #5b89fe !important;
-  background: #C0C4CC !important;
+.el-menu-item {
+  height: 50px;
+  margin-left: 10px;
+  width: 220px;
+  margin-top: 5px;
 }
 
 .menu {
-  height: 88.8vh;
+  margin-top: 10px;
+  height: 87.3vh;
 }
 
 .el-menu {
-  width: 200px;
+  width: 240px;
 }
 
 .el-image {
@@ -234,24 +276,66 @@ html, body {
   padding: 0;
 }
 
-.el-submenu__title {
-  padding-left: 30px !important;
-}
-
-.el-menu-item:last-child:hover{
-  .el-dropdown{
-    visibility: visible;
-    float: right;
+::v-deep .el-tree-node:focus > .el-tree-node__content {
+  .el-menu .el-menu-item.is-active {
+    @include font_color("aside_text-color");
+    @include background_color("aside_color");
   }
+
+  @include font_color("aside_text-color");
+  border-radius: 0.8em !important;
+  background: #e7ebed !important;
 }
 
-.el-dropdown{
-  visibility: hidden;
-  float: right;
+::v-deep .el-tree-node__content:hover {
+  @include font_color("aside_text-color");
+  border-radius: 0.8em !important;
+  background: #e7ebed !important;
+  transition: background 1s, color 0.6s;
 }
 
-.el-icon-more:hover{
-  background-color: #e1e6ed;
+::v-deep .el-tree-node__content {
+  margin-left: 10px;
+  height: 50px !important;
+}
+
+.el-tree {
+  width: 95%;
+}
+
+.el-icon-plus:hover, .el-icon-more:hover {
+  background-color: white;
   border-radius: 0.4em;
+}
+
+.el-icon-more, .el-icon-plus {
+  padding: 5px;
+}
+
+.addWorkspace:hover {
+  @include font_color("aside_text-color");
+  border-radius: 0.8em;
+  background: #e7ebed !important;
+}
+
+.addWorkspace {
+  width: 220px;
+  margin-left: 10px;
+  transition: background 1s, color 0.6s;
+}
+
+.el-button--text {
+  padding-left: 20px;
+  padding-right: 20px;
+  @include font_color("aside_text-color");
+}
+
+.custom-tree-node {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 14px;
+  padding-right: 8px;
 }
 </style>
